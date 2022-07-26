38abdc9e54583946cb8f774755980340935eef3f
Gabriela Ferrara <105033997+starykt@users.noreply.github.com>
2022-07-26 15:04:42 -0300

    Listagem de informações
    Inserção de v-for e array para fazer um ciclo de apresentação dos dados na página.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,20 @@
 
 <div>
   <h1>{{ titulo }}</h1>   <!-- ou <h1 v-text="titulo"> -->
-    
-  <img :src="foto.url" :alt="titulo.url"> <!-- o vue não aceita interpolação em atributo
-                                                           por isso se utiliza o v-bind: ou só : -->
+
+  <ul>
+    <li v-for="foto of fotos"> <!-- v-for funciona como um while ou for para passar por cada valor do nosso array -->
+                               <!-- foto (é o paramêtro que está passando no img) e fotos (o nome do array) -->
+
+      <img :src="foto.url" :alt="foto.titulo">
+    </li>
+
+  </ul>
+
+   <!-- o vue não aceita interpolação em atributo
+      por isso se utiliza o v-bind: ou só : -->
+  
+  
 </div>
 
 
@@ -16,10 +27,17 @@ export default {
   data(){
     return {
       titulo: 'Alurapic',
-      foto:{
+      fotos: [
+        {
         url: 'https://i.pinimg.com/564x/d5/3f/f1/d53ff1e5e45113fe77a362b7086f4ea7.jpg',
         titulo: 'Cachorro'
-      }
+        },
+
+        {
+        url: 'https://i.pinimg.com/564x/6f/a4/77/6fa477ff84a59abe2f7d07f13099326c.jpg',
+        titulo: 'Gatinho'
+        }
+      ]   
     }
   }
 }
