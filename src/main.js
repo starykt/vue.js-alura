fcce99bacb446d1040e6de809e8ca90e85a9279f
Gabriela Ferrara <105033997+starykt@users.noreply.github.com>
2022-07-26 17:10:17 -0300

    Instalação do Vue Resource
    Instalação do Vue Resource para aplicar a API.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource) // puxando a importação do vue resource que foi instalado para o global Vue 
+                     // para integração com API.
 
 new Vue({
   el: '#app',
