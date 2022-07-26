6d47d72214e19d66ba549f121d26103cdf7af471
Gabriela Ferrara <105033997+starykt@users.noreply.github.com>
2022-07-26 13:56:38 -0300

    Adequando template
    Iniciação da inserção de dados na página para teste.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,28 @@
 <template>
 
+<div>
+  <h1>{{ titulo }}</h1>   <!-- ou <h1 v-text="titulo"> -->
+    
+  <img :src="foto.url" :alt="titulo.url"> <!-- o vue não aceita interpolação em atributo
+                                                           por isso se utiliza o v-bind: ou só : -->
+</div>
+
+
 </template>
 
 <script>
+
+export default {
+  data(){
+    return {
+      titulo: 'Alurapic',
+      foto:{
+        url: 'https://i.pinimg.com/564x/d5/3f/f1/d53ff1e5e45113fe77a362b7086f4ea7.jpg',
+        titulo: 'Cachorro'
+      }
+    }
+  }
+}
 
 </script>
 
